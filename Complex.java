4d52feaccf50b2d8e04eb261341288148fd8221f
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2023-01-29 01:00:27 +0300

    add doc for complex class

--- Complex.java
+++ Complex.java
@@ -1,33 +1,59 @@
+/**
+ * Класс комплексных чисел 
+ * @author the_best_earth_spirit_player
+ * */
 public class Complex {
-  double real;
-  double imag;
+  /** Поле действительной части комплексного числа */
+  public double real;
+  /** Поле мнимой части комплексного числа */
+  public double imag;
 
+  /** Конструктор, создающий нулевое комплексное число 
+   * @see Complex#Complex(double, double)
+   * */
   Complex () { 
     this.real = 0;
     this.imag = 0;
   }
-
+  
+  /** Конструктор, создающий комплексное число с определенным значением
+   * @param real действительная часть
+   * @param imag мнимая часть
+   * @see Complex#Complex()
+   * */
   Complex (double real, double imag) { 
     this.real = real;
     this.imag = imag;
   }
 
+  /** Прибавление к комплексному числа другого
+   * @param dig слагаемое
+   * */
   public void add (Complex dig) { 
     this.real += dig.real;
     this.imag += dig.imag;
   }
   
+  /** Вычитание из комплексного числа другого
+   * @param dig вычитаемое
+   * */
   public void subtraction (Complex dig) { 
     this.real -= dig.real;
     this.imag -= dig.imag;
   }
 
+  /** Умножение комплексного числа на другое
+   * @param dig множитель
+   * */
   public void multiplication (Complex dig) {
     Complex tmp = new Complex(this.real*dig.real - this.imag*dig.imag,
         this.real*dig.imag + dig.real*this.imag);
     this.copy(tmp);
   }
 
+  /** Деление комплексного числа на другое
+   * @param dig делитель
+   * */
   public void division (Complex dig) {
     double tmp_real = (this.real*dig.real + this.imag*dig.imag)/(dig.real*dig.real + dig.imag*dig.imag);
     double tmp_img = (this.imag*dig.real - this.real*dig.imag)/(dig.real*dig.real + dig.imag*dig.imag);
@@ -35,15 +61,20 @@ public class Complex {
     this.copy(tmp);
   }
   
-  public void conjugate () {
-    imag = -imag;
-  }
-
+  /** Копирование комплексного числа 
+   * @param dig копируемое
+   * */
   public void copy (Complex dig) {
     this.real = dig.real;
     this.imag = dig.imag;
   }
+  
+  /** Получение сопряженного комплексному */
+  public void conjugate () {
+    imag = -imag;
+  }
 
+  /** Вывод тригонометрической формы комплексного числа */
   public void trigonometric_form () {
     double r = Math.sqrt(real*real + imag*imag);
     double cos = real/r;
@@ -51,6 +82,7 @@ public class Complex {
     System.out.println(r + "*(" + cos + (sin >= 0 ? "+" : "") + sin + "*i)");
   }
 
+  /** Вывод комплексного числа  */
   public void print () {
     System.out.println(real + (imag >= 0 ? "+" : "") + imag + "*i");
   }
