ccdb9f0543d5eab77180bca2b68b3a5ee5dedd31
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2023-01-29 12:06:17 +0300

    add input method

--- Matrix.java
+++ Matrix.java
@@ -1,17 +1,32 @@
+import java.util.Scanner;
+
 public class Matrix {
   Complex[][] matrix;
+  private int rows;
+  private int columns;
 
-  Matrix (int n) {
-    this.matrix = new Complex[n][n];
+  Matrix (int ROWS_AND_COLUMNS_NUMBER) {
+    this.matrix = new Complex[ROWS_AND_COLUMNS_NUMBER][ROWS_AND_COLUMNS_NUMBER];
+    this.rows = ROWS_AND_COLUMNS_NUMBER;
+    this.columns = ROWS_AND_COLUMNS_NUMBER;
     this.fill_matrix();
   }
 
-  Matrix (int n, int m) {
-    this.matrix = new Complex[n][m];
+  Matrix (int ROWS_NUMBER, int COLUMNS_NUMBER) {
+    this.matrix = new Complex[ROWS_NUMBER][COLUMNS_NUMBER];
+    this.rows = ROWS_NUMBER;
+    this.columns = COLUMNS_NUMBER;
     this.fill_matrix();
   }
 
   public void fill_matrix () {
-
+    Scanner in = new Scanner(System.in);
+    for (int i = 0; i < this.rows; ++i) {
+      for (int j = 0; j < this.columns; ++j) {
+        matrix[i][j] = Complex.input(in);
+      }
+    }
+    in.close();
   }
+
 }
